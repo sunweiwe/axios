0f2dca718997f893c881ffd9be4b91132291f19d
sunxiaowen <734533312@qq.com>
2020-11-02 22:35:17 +0800

    :art: handle url

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+//
+
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+// 请求结构参数类型
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
